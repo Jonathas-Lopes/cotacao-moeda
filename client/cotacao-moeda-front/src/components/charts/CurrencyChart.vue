9609bb58f64936e0f26fc483d85c270e223f4fd7
Jonathas-Lopes <jonathaslopes20@gmail.com>
2021-12-06 20:21:33 -0300

    Criando componentes principais

---
+++ client/cotacao-moeda-front/src/components/charts/CurrencyChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="width:100%; ">
+    <line-chart :chart-data="Currency" :options="options"></line-chart>
+    
+  </div>
+</template>
+
+<script>
+import LineChart from "./templates/LineChart";
+
+export default {
+  components: {
+    LineChart,
+  },
+  data() {
+    return {      
+      options: { responsive: true, maintainAspectRatio: false },
+    };
+  },
+  mounted() {
+   
+  },
+  methods: {
+    
+  },
+  computed:{
+    Currency(){      
+        return this.$store.state.chartValue      
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/cotacao-moeda-front/src/views/CotacaoReal.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-content>
+    <v-container fluid
+      >
+      <v-row>
+        <v-col><Menu /></v-col> 
+        <v-col md="10" lg="10"><Chart /></v-col>
+      </v-row
+    ></v-container>
+    
+  </v-content>
+</template>
+
+<script>
+// @ is an alias to /src
+import CurrencyChart from "@/components/charts/CurrencyChart";
+import SideMenu from '@/components/SideMenu'
+
+export default {
+  name: "Home",
+  components: {
+    Menu:SideMenu,
+    Chart: CurrencyChart,
+  },
+};
+</script>
