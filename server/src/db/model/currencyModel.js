51636fcff8c15d3f90623f8397c694d2648c3a5b
Jonathas-Lopes <jonathaslopes20@gmail.com>
2021-12-27 18:23:09 -0300

    Atendendo as seguintes solicitações: Alteração do método 'map'na função insert; Alteração do metodo 'updateOne' na função insert; Validação dos dados da integração com bacen.

--- server/src/db/model/currencyModel.js
+++ server/src/db/model/currencyModel.js
@@ -1,64 +1,46 @@
 const mongoose = require("mongoose");
 const CurrencySchema = require("../schema/currencySchema")
 const CurrencyModel = mongoose.model("currency", CurrencySchema);
+const bacenValidator = require('../../utils/validators/bacenValidator')
 
 
 const currency = {
-    
-    find: async function(){
+
+    find: async function () {
         return await CurrencyModel.find({}).limit(50)
     },
     insert: async function (currencyNames, currencyObj) {
-        const schemas = currencyNames.map((key, index) => {
-            const values = {
-                'currencyName': currencyObj[key].code,
-                'high': new Array(currencyObj[key].high),
-                'low': new Array(currencyObj[key].low),
-                'date': new Array(currencyObj[key].create_date)
+        const registers = currencyNames.map((key) => ({
+
+            'currencyName': currencyObj[key].code,
+            'high': new Array(currencyObj[key].high),
+            'low': new Array(currencyObj[key].low),
+            'date': new Array(currencyObj[key].create_date)
+        }))
+
+        if (!bacenValidator.validate(registers)) {
+            for (let register in registers) {
+
+
+                const currency = await CurrencyModel.findOne({ currencyName: registers[register].currencyName })
+
+
+
+                if (!currency) {
+                    await CurrencyModel.create(registers[register]);
+                }
+                else {
+
+
+                    await CurrencyModel.updateOne(
+                        { currencyName: registers[register].currencyName },
+                        { $push: { high: registers[register].high, low: registers[register].low, date: registers[register].date } }
+
+
+                    )
+                }
             }
-            return values
-        })
-
-        for (let schema in schemas) {
-
-
-            const currency = await CurrencyModel.findOne({ currencyName: schemas[schema].currencyName })
-
-
-
-            if (!currency) {
-                await CurrencyModel.create(schemas[schema]);
-            }
-            else {
-
-
-                await CurrencyModel.updateOne(
-                    { currencyName: schemas[schema].currencyName },
-                    { $push: { high: schemas[schema].high } }
-
-
-                );
-                await CurrencyModel.updateOne(
-                    { currencyName: schemas[schema].currencyName },
-                    { $push: { low: schemas[schema].low }, }
-
-
-                );
-                await CurrencyModel.updateOne(
-                    { currencyName: schemas[schema].currencyName },
-                    { $push: { date: schemas[schema].date }, }
-
-
-                );
-
-            }
-
         }
-
-
-
-
-
     }
 }
 
