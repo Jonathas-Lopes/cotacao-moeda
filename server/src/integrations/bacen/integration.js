bf00e5aa2731bc34ba7437ce618e6312e6e72e13
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-01-15 20:01:20 -0300

    correção slint

--- server/src/integrations/bacen/integration.js
+++ server/src/integrations/bacen/integration.js
@@ -1,28 +1,20 @@
-const fetch = require("node-fetch");
-const scheduler = require("node-cron")
-const currency = require("../../db/model/currencyModel");
-
+const fetch = require('node-fetch');
+const scheduler = require('node-cron');
+const currency = require('../../db/model/currencyModel');
 
 const getCurrencies = async function integration(currencies, socket) {
 
-    scheduler.schedule('* * * * *', async function () {
+  scheduler.schedule('* * * * *', async function () {
 
-        
-        const response = await fetch(`https://economia.awesomeapi.com.br/last/${currencies}`);
-        const data = await response.json()
-        currency.insert(Object.keys(data), data)
+    const response = await fetch(`https://economia.awesomeapi.com.br/last/${currencies}`);
+    const data = await response.json();
+    currency.insertCurrency(Object.keys(data), data);
 
-        console.log('listen')
-        socket.emit("currencies", await currency.find({}));
+    console.log('listen');
+    socket.emit('currencies', await currency.listCurrencies(true));
 
+  });
 
-    });
-
-
-}
-
-
-
-
+};
 
 module.exports = getCurrencies;
