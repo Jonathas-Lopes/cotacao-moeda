ddf3979f93d670121afa93fe4d0a3f34160f133a
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-01-15 20:25:20 -0300

    alterado para validar todos os dados da API ao invés de um dado por vez.

--- server/src/utils/validators/bacenValidator.js
+++ server/src/utils/validators/bacenValidator.js
@@ -1,20 +1,31 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
 const validation = {
-    validator: Joi.object({
-        'currencyName': Joi.string().alphanum().max(3).required(),
-        'high': Joi.number().precision(4).required(),
-        'low': Joi.number().precision(4).required(),
-        'date': Joi.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s([0-9]+(:[0-9]+)+)$/).required()
-    }),
-    validate: (object) => {
-        try {
-            validation.validator.validate(object)
-        }
-        catch (err) {
-            return err
-        }
-    }
-}
+  validator: Joi.object({
+    currencyName: Joi.string().alphanum().max(3).required(),
+    high: Joi.number().precision(4).required(),
+    low: Joi.number().precision(4).required(),
+    date: Joi.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s([0-9]+(:[0-9]+)+)$/).required()
+  }),
+  hasInvalidField: (object) => {
+    const validacao = Object.keys(object).map((key) => {
+      try {
+        validation.validator.validate(
+          {
+            currencyName: object[key].code,
+            high: object[key].high,
+            low: object[key].low,
+            date: object[key].create_date
+          }
+        );
+      }
+      catch (err) {
+        return 'error';
+      }
 
-module.exports = validation
+    }).includes('error');
+
+  }
+};
+
+module.exports = validation;
