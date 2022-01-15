e6c4b00e351178ab5d4623caf4794a82d84206fa
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-01-15 19:47:33 -0300

    atendendo comentarios acrescentar dotenv

--- server/src/db/connection/connection.js
+++ server/src/db/connection/connection.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose")
-require('dotenv').config() 
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
 
 const connectionConfig = {
-    connect : async function () {
-        await mongoose.connect(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLLECTION}`);
+  connect : async function () {
+    await mongoose.connect(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLLECTION}`);
 
-    },
-    db: mongoose.connection
-}
+  },
+  db: mongoose.connection
+};
 module.exports = connectionConfig;
