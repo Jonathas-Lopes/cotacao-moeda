c7cab33a6ef687da5cc53e4fbdf76d6b8ed20168
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-01-15 23:50:50 -0300

    Eliminando variaveis desnecessárias.

--- server/src/utils/validators/bacenValidator.js
+++ server/src/utils/validators/bacenValidator.js
@@ -5,27 +5,24 @@ const validation = {
     currencyName: Joi.string().alphanum().max(3).required(),
     high: Joi.number().precision(4).required(),
     low: Joi.number().precision(4).required(),
-    date: Joi.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s([0-9]+(:[0-9]+)+)$/).required()
+    date: Joi.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s([0-9]+(:[0-9]+)+)$/).required(),
   }),
   hasInvalidField: (object) => {
-    const validacao = Object.keys(object).map((key) => {
+    Object.keys(object).map((key) => {
       try {
         validation.validator.validate(
           {
             currencyName: object[key].code,
             high: object[key].high,
             low: object[key].low,
-            date: object[key].create_date
-          }
+            date: object[key].create_date,
+          },
         );
-      }
-      catch (err) {
+      } catch (err) {
         return 'error';
       }
-
     }).includes('error');
-
-  }
+  },
 };
 
 module.exports = validation;
