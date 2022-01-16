97b36e349b04cd497a60883f0d237b964beefa2a
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-01-15 23:48:49 -0300

    alteração eslint

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const logger = require('morgan');
+
 const app = express();
 app.use(logger('dev'));
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error')});
+  res.render('error');
+});
 module.exports = app;
