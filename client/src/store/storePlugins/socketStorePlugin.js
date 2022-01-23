e9883da30914307b1abb7f89c889eef6d126bd70
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-01-22 21:30:15 -0300

    Criando plugin websocket para uso no Vuex

---
+++ client/src/store/storePlugins/socketStorePlugin.js
@@ -0,0 +1,39 @@
+export default function createWebSocketPlugin (socket) {
+  return store => {
+    socket.on('currencies', data => {
+      store.commit('SetSideMenuItens', data.map(item=>item.currencyName));
+      store.commit('SetCurrency', data.map(element=>({currencyName:element.currencyName, chartData:{
+        options: {
+          responsive: true,
+        },
+
+        labels: element.date,
+        datasets: [
+          {
+            label: `${element.currencyName} high`,
+            backgroundColor: "#000000",
+            data: element.high,
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+          },
+          {
+            label: `${element.currencyName} low`,
+            backgroundColor: "#000000",
+            data: element.low,
+            fill: false,
+            borderColor: "rgb(75, 192, 0)",
+          }
+        ],
+      }})))
+    })
+    socket.on('updateCurrencies', data=>{
+      store.commit('updateCurrency', data)
+
+    })
+    store.subscribe(mutation => {
+      if (mutation.type === 'UPDATE_DATA') {
+        socket.emit('update', mutation.payload)
+      }
+    })
+  }
+}
